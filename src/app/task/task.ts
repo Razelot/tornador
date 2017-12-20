bf807a0646af5c8613dda6ff666e7d46610d8fed
Carlo Putra <carlo@kreasikuliner.onmicrosoft.com>
2017-12-20 11:33:22 +0800

    kontol

--- src/app/task/task.ts
+++ src/app/task/task.ts
@@ -5,5 +5,9 @@ export interface Task {
     date_created: Date;
     date_due: Date;
     members_assigned: {};
+<<<<<<< HEAD
     tags: {"kontol something"};
+=======
+    tags: {"cunt something"};
+>>>>>>> 1810bb7095f053c97e1f7681fa7758629684e8ed
   }
