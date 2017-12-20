2697393d96a346b85b3d612c8f3e633e4e793692
Ezar Enanda <razelot@gmail.com>
2017-12-20 11:18:00 +0800

    test

---
+++ src/app/task/task.ts
@@ -0,0 +1,9 @@
+export interface Task {
+    title: string;
+    description: string;
+    status: string;
+    date_created: Date;
+    date_due: Date;
+    members_assigned: {};
+    tags: {};
+  }
